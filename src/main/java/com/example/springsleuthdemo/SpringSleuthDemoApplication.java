55695300f10b1cce45a60b8ebef3b8421dd3166a
Iván Álvarez Pereira <ialvarezpereira@gmail.com>
2019-11-26 16:54:41 +0100

    first commit

---
+++ src/main/java/com/example/springsleuthdemo/SpringSleuthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsleuthdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSleuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSleuthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsleuthdemo/SpringSleuthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsleuthdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSleuthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
